1be9a97a9f6eb5ecd8bb5dc1d88663054921a8ac
nankiewiczka <karolina.nankiewicz@gmail.com>
2018-06-15 23:15:27 +0200

    changes

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
 public class Controller {
+    @FXML
+    private Button addButton;
+    public void clickAddButton(ActionEvent actionEvent) {
+
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Dodaj wydarzenie ");
+        dialog.setContentText("Podaj dzien");
+
+
+        Optional<String> result = dialog.showAndWait();
+
+
+
+        result.ifPresent(name -> System.out.println("wydarzenia: " + name));
+
+    }
+
+
 }
