52829942bb5adcc75d477e70bbc46d227ac80b33
DESKTOP-JADVLLF\Mariusz <mariusz1614@poczta.onet.pl>
2018-06-16 14:26:01 +0200

    Test

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,43 +1,40 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.awt.event.ActionEvent;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
         DataBase dataBase = new DerbyDataBase();
         dataBase.createConnectionToDerby();
+        System.out.println("aa");
+
         /*dataBase.createUserTable();
         dataBase.addRecordToUserTable(1, "kasia");
-        dataBase.addRecordToUserTable(4, "karolina");
-        dataBase.showUserTable();*/
+        dataBase.addRecordToUserTable(4, "karolina");*/
 
-        /*dataBase.createEventTable();
-        dataBase.addRecordToEventTable(1, 2, 3, 4, "wyniesc smieci");
-        dataBase.addRecordToEventTable(2, 2, 6, 7, "pozmywac naczynia");
-        dataBase.showEventTable();*/
+        //dataBase.createEventTable();
+        
+        //dataBase.addRecordToEventTable(1, 2, "Poniedzia³ek", 4, 15, "wyniesc smieci");
+        //dataBase.addRecordToEventTable(2, 2, "Wtorek", 7, 20, "pozmywac naczynia");
+        
+		System.out.println(dataBase.getAllUsers());
+		System.out.println(dataBase.getAllEvents());
 //        dataBase.backUpDatabase();
 //        dataBase.restoreDatabase();
 
-        FXMLLoader loader = new FXMLLoader();
+		FXMLLoader loader = new FXMLLoader();
         loader.setController(new Controller());
-        Pane root = loader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Harmonogram ZajeÄ‡ Rodzinnych");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-
-
         //test pusha brancha
         //ostatni test
     }
