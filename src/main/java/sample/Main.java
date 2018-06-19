87fbb9353230f783b720cdbe656697d9a357a551
Krzysztof Pętlak <krzysiek251p@gmail.com>
2018-06-20 00:56:08 +0200

    Revert "resizeable event buttons done"

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -17,7 +17,7 @@ public class Main extends Application {
 
         FXMLLoader loader = new FXMLLoader();
         loader.setController(new Controller());
-        AnchorPane root = loader.load(getClass().getResource("sample.fxml"));
+        ScrollPane root = loader.load(getClass().getResource("sample.fxml"));
         root.getStylesheets().add(String.valueOf(getClass().getResource("/styles/style1.css")));
         primaryStage.setTitle("Harmonogram Zajeć Rodzinnych");
         primaryStage.setScene(new Scene(root, 850, 700));
