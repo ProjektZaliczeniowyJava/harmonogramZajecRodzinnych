8e436f7076d241677e3990a1e9f75a758d2ef18e
Krzysztof <krzysiek251p@gmail.com>
2018-06-19 14:55:32 +0200

    rest userTest

--- src/test/java/sample/UserTest.java
+++ src/test/java/sample/UserTest.java
@@ -10,20 +10,30 @@ public class UserTest {
     @Test
     public void getId() {
         User user = new User(2, "Krzysztof");
-        Assert.assertEquals(user.getId(), 2);
+        Assert.assertEquals(2, user.getId());
         user = new User("Krzysztof");
-        Assert.assertEquals(user.getId(), 0);
+        Assert.assertEquals(0, user.getId());
     }
 
     @Test
     public void setId() {
+        User user = new User("Karolina");
+        user.setId(987);
+        Assert.assertEquals(987, user.getId());
     }
 
     @Test
     public void getName() {
+        User user = new User("Kasia");
+        Assert.assertEquals("Kasia", user.getName());
+        user = new User(123, "Mariusz");
+        Assert.assertEquals("Mariusz", user.getName());
     }
 
     @Test
     public void setName() {
+        User user = new User("Tomasz");
+        user.setName("Kamil");
+        Assert.assertEquals("Kamil", user.getName());
     }
 }
